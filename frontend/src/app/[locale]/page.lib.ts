5aa6c895d7472c4e7f353fce35d309f2b2b7d460
Riccardo Giovarelli <riccardo.giovarelli@gmail.com>
2026-03-14 01:10:19 +0100

    feat: Utility for api base url

--- frontend/src/app/[locale]/page.lib.ts
+++ frontend/src/app/[locale]/page.lib.ts
@@ -1,19 +1,24 @@
+import { API_BASE_URL } from "@/lib/api/api.lib";
+
+/**
+ * Fetches localized home page data from the backend.
+ * * @param {string} locale - The target language code.
+ * @returns {Promise<any | null>} The parsed JSON data from the API, or null if the request fails.
+ */
 export async function getHomeData(locale: string) {
   try {
-    const url = `http://127.0.0.1:1337/api/home?locale=${locale}`;
-
-    const res = await fetch(url, {
+    const res = await fetch(`${API_BASE_URL}/api/home?locale=${locale}`, {
       cache: "no-store",
     });
 
     if (!res.ok) {
-      console.error("Strapi ha risposto con un errore:", res.status);
+      console.error("[HOME] BE responded with an error:", res.status);
       return null;
     }
 
     return res.json();
   } catch (error) {
-    console.error("Impossibile connettersi a Strapi:", error);
+    console.error("[HOME] Unable to connect to BE:", error);
     return null;
   }
 }
